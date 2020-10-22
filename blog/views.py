3f669b28768ef7ced5db74e0b926585af8e6a2bf
Thomas <tn@riskconcile.com>
2020-10-22 16:29:10 +0200

    Refactored list to ClassView

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.views.generic.list import ListView
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
 
 
-def post_list(request):
-    posts = Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+@method_decorator(login_required, name='dispatch')
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = Post.objects.filter(
+            published_date__lte=timezone.now()
+        ).order_by('published_date')
+        return queryset
 
 
 def post_detail(request, pk):
